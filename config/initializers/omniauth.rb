6b291c07e01ceb6c46e81cf340a03e76e0c639b8
Jan Rychtar <honza.rychtar@gmail.com>
2019-10-28 17:06:00 +0100

    skip jwt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret']
+  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], skip_jwt: true
   provider :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'], scope: 'public_profile,email'
 end
